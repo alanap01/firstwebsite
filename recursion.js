0ba89b1024cd369c9296c70382b356674d817fc0
alanap01 <parkera2026@tvs.org>
2024-03-28 12:10:48 -0500

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -4,5 +4,14 @@ createCanvas(1000, 800);
 }
 
 function draw(){
-    line(0, 0, width, height);
+    tri(width/2, 0, width, height);
+    // triangle(width/2, 0, width, height, 0, height);
+}
+function tri(x, y, w, h){
+    triangle(x, y, x+w/2, y+h, x-w/2, y+h);
+    if(w>5){
+    tri(x, y, w/2, h/2);
+    tri(x+w/4, y+h/2, w/2, h/2);
+    tri(x-w/4, y+h/2, w/2, h/2);
+    }
 }
