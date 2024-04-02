580a0664dfd8b3221c6bf17b52f4d23e7eef0cfc
alanap01 <parkera2026@tvs.org>
2024-04-02 13:37:28 -0500

    d

--- recursion.js
+++ recursion.js
@@ -9,7 +9,7 @@ function draw(){
 }
 function tri(x, y, w, h){
     triangle(x, y, x+w/2, y+h, x-w/2, y+h);
-    if(w>5){
+    if(w>4){
     tri(x, y, w/2, h/2);
     tri(x+w/4, y+h/2, w/2, h/2);
     tri(x-w/4, y+h/2, w/2, h/2);
